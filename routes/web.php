fc936e845248b0bd52dc3a840c58a213c18633b3
relvynjoshua <cagadas.relvynjoshua@gmail.com>
2023-05-20 12:03:18 -0700

    Lab 1 - Final

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+$router->get('/users',['uses' => 'TeacherController@getUsers']);
+});
+
+$router->get('/GETusers',['uses' => 'UserController@getUsers']);
+$router->get('/SEARCHusers/{id}',['uses' => 'UserController@searchUsers']);
+$router->post('/ADDusers',['uses' => 'UserController@addUsers']);
+$router->patch('/UPDATEusers/{id}',['uses' => 'UserController@updateUser']);
+$router->delete('/DELETEusers/{id}',['uses' => 'UserController@deleteUser']);
+
